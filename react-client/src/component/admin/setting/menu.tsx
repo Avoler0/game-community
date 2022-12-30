b7060c78ca04f17254c3dddc385ebb28bfd1fad3
Avoler0 <jys1391@gmail.com>
2022-12-30 23:58:58 +0900

    Feat: 어드민 페이지 기본 구상

--- react-client/src/component/admin/setting/menu.tsx
+++ react-client/src/component/admin/setting/menu.tsx
@@ -2,11 +2,43 @@ import { useState } from 'react';
 
 
 const AdminMenuSetting = () => {
-
-  
+  const [selectCategory,setSelectCategory] = useState(null);
+  const category = ["직업 게시판","정보 게시판"];
+  const menulist = [
+    {
+      category:'직업 게시판',
+      name:'전사',
+    },
+    {
+      category:'직업 게시판',
+      name:'마법사',
+    },
+    {
+      category:'정보 게시판',
+      name:'게임 뉴스',
+    },
+    {
+      category:'정보 게시판',
+      name:'게임 업데이트',
+    },
+  ]
+  function onChange(e:any){
+    setSelectCategory(e.target.value)
+  }
   return (
     <div>
-      메뉴 부분 입니다.
+      <select name='menu_category' onChange={onChange}>
+        {category.map((data)=>{
+          return <option key={data} value={data}>{data}</option>
+        })}
+      </select>
+      <div>
+        {menulist.map((data)=>{
+          if(data.category === selectCategory){
+            return <div>{data.name}</div>
+          }
+        })}
+      </div>
     </div>
   )
 }
